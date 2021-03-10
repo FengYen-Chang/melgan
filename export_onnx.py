befff1ef23d02d8ef9d0a28b60322d61e5a4bfa7
FengYen-Chang <a60207john@gmail.com>
2021-03-10 02:13:20 +0000

    convert the graph to onnx format w/ fixed input size.

---
+++ export_onnx.py
@@ -0,0 +1,23 @@
+import torch
+import torch.onnx
+import numpy as np
+from model.generator import Generator
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+mel = torch.randn(1, 80, 200)
+
+if torch.cuda.is_available():
+    melgan = torch.hub.load('seungwonpark/melgan', 'melgan')
+    print('Moving data & model to GPU')
+    melgan = melgan.cuda()
+    mel = mel.cuda()
+else:
+    print('Moving data & model to CPU')
+    melgan = Generator(80)
+    checkpoint = torch.hub.load_state_dict_from_url('https://github.com/seungwonpark/melgan/releases/download/v0.3-alpha/nvidia_tacotron2_LJ11_epoch6400.pt', map_location="cpu")
+    melgan.load_state_dict(checkpoint["model_g"])
+    melgan.eval(inference=True)
+
+torch.onnx.export(melgan, mel, "melgan.onnx")
+
